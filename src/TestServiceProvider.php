f0120de875d6bcb85d9009685609e60cd790851f
huwei <wei.hu@haoqitech.com.cn>
2016-11-28 14:32:43 +0800

    修改，public文件和view文件的，publish语句

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -22,28 +22,17 @@ class TestServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views/scms/api', 'test4');
 
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/lrts'),
-            __DIR__.'/views' => base_path('resources/views/scms/admin'),
-            __DIR__.'/views' => base_path('resources/views/scms/api'),
+            __DIR__.'/views/lrts' => base_path('resources/views/lrts'),
+            __DIR__.'/views/scms' => base_path('resources/views/scms'),
             __DIR__.'/config/filesy.php' => config_path('filesy.php'),
-
         ]);
         $this->publishes([
-            __DIR__.'/public/assets' => public_path('assets/bootstrap/css'),
-            __DIR__.'/public/assets' => public_path('assets/bootstrap/fonts'),
-            __DIR__.'/public/assets' => public_path('assets/css'),
-            __DIR__.'/public/assets' => public_path('assets/jquery/plugin/fileUploadJs'),
-            __DIR__.'/public/assets' => public_path('assets/jquery/plugin/timeConversion'),
-            __DIR__.'/public/assets' => public_path('assets/jquery'),
-            __DIR__.'/public/assets' => public_path('assets/sass'),
-            __DIR__.'/public/assets' => public_path('assets/scms/image/backimage'),
-            __DIR__.'/public/assets' => public_path('assets/scms/image/uploads/bigimg'),
-            __DIR__.'/public/assets' => public_path('assets/scms/image/uploads/smallimg'),
-            __DIR__.'/public/assets' => public_path('assets/web/css'),
-            __DIR__.'/public/assets' => public_path('assets/web/html'),
-            __DIR__.'/public/assets' => public_path('assets/web/images'),
-            __DIR__.'/public/assets' => public_path('assets/web/js'),
-
+            __DIR__.'/public/assets/bootstrap' => public_path('assets/bootstrap'),
+            __DIR__.'/public/assets/css' => public_path('assets/css'),
+            __DIR__.'/public/assets/jquery' => public_path('assets/jquery'),
+            __DIR__.'/public/assets/sass' => public_path('assets/sass'),
+            __DIR__.'/public/assets/scms' => public_path('assets/scms'),
+            __DIR__.'/public/assets/web' => public_path('assets/web'),
         ], 'public');
     }
 
